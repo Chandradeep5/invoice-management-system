e1d003b26f1a029781a93ac7a5060451a888363a
Chandra deep <chandradeepreddy4@gmail.com>
2026-05-28 22:42:22 +0530

    Initialized React frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1>
+        Invoice Management System
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
+
