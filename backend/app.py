e3921f1a3cc9988d1b31f88ce7618bfac6f1d9cc
Chandra deep <chandradeepreddy4@gmail.com>
2026-05-28 12:48:01 +0530

    Integrated Swagger documentation

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,24 @@
+
 from flask import Flask
+from flasgger import Swagger
 
 app = Flask(__name__)
 
+# Swagger configuration
+swagger = Swagger(app)
+
 @app.route("/")
 def home():
-    return {"message": "Invoice Management System API Running"}
+    """
+    Home API
+    ---
+    responses:
+      200:
+        description: API is running successfully
+    """
+    return {
+        "message": "Invoice Management System API Running"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
